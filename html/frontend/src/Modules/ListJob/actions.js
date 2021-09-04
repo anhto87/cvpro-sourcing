c7a1a9541f7a30a3f02cdfe19b558bc611a897bd
Đô Trần <dotran162@gmail.com>
2021-09-04 10:52:33 +0700

    add limit suggest list

--- html/frontend/src/Modules/ListJob/actions.js
+++ html/frontend/src/Modules/ListJob/actions.js
@@ -1,59 +1,60 @@
 /***
- * 
- * 
- * 
+ *
+ *
+ *
  */
-export const UPDATE_FORM = "UPDATE_FORM";
-export const UPDATE_PAGE = "UPDATE_PAGE";
-export const LOAD_COMPLETE = "LOAD_COMPLETE";
-export const LOADING = "LOADING";
-export const RESET = "RESET";
-export const JOB_SUGGEST = "JOB_SUGGEST";
+export const UPDATE_FORM = 'UPDATE_FORM';
+export const UPDATE_PAGE = 'UPDATE_PAGE';
+export const LOAD_COMPLETE = 'LOAD_COMPLETE';
+export const LOADING = 'LOADING';
+export const RESET = 'RESET';
+export const JOB_SUGGEST = 'JOB_SUGGEST';
 
 export const updateFormValues = (payload) => {
     return {
         type: UPDATE_FORM,
         payload
-    }
-}
+    };
+};
 
 export const updatePageAction = (payload) => {
     return {
         type: UPDATE_PAGE,
         payload
-    }
-}
+    };
+};
 
 export const loadCompletedAction = ({ jobs, totalJobs, page, totalPage, limit }) => {
     return {
         type: LOAD_COMPLETE,
         payload: { jobs, totalJobs, page, totalPage, limit }
-    }
-}
+    };
+};
 
 export const loading = (payload) => {
     return {
         type: LOADING,
         payload
-    }
-}
+    };
+};
 
 export const resetAction = () => {
     return {
         type: RESET
-    }
-}
+    };
+};
 
 export const jobsSuggestAction = (jobs) => {
     return {
         type: JOB_SUGGEST,
         payload: convertSuggestJob(jobs)
-    }
-}
+    };
+};
 
 const convertSuggestJob = (jobs) => {
     const items = [];
-    for (let index = 0; index < jobs.length; index++) {
+    const maxLength = jobs.length > 10 ? 10 : jobs.length;
+    for (let index = 0; index < maxLength; index++) {
         const job = jobs[index];
         const _id = job?.id;
         const jobId = job?.id;
@@ -78,15 +79,35 @@ const convertSuggestJob = (jobs) => {
         const onlineDate = job?.updated_at;
         const publishedDate = job?.created_at;
         const expiredDate = job?.deadline;
-        items.push({ expiredDate, publishedDate, link, onlineDate, jobType, jobLocations, skills, benefits, locations, _id, domain, jobId, jobTitle, companyLogo, company, companyId, jobDescription, jobRequirement, salary })
+        items.push({
+            expiredDate,
+            publishedDate,
+            link,
+            onlineDate,
+            jobType,
+            jobLocations,
+            skills,
+            benefits,
+            locations,
+            _id,
+            domain,
+            jobId,
+            jobTitle,
+            companyLogo,
+            company,
+            companyId,
+            jobDescription,
+            jobRequirement,
+            salary
+        });
     }
-    return items
-}
+    return items;
+};
 
 const convertSalary = (salary, salary_upto, salary_currency) => {
     const formatter = new Intl.NumberFormat('en-US', {
         style: 'currency',
-        currency: salary_currency,
+        currency: salary_currency
     });
 
     if (typeof salary === 'number' && typeof salary_upto === 'number') {
@@ -97,4 +118,4 @@ const convertSalary = (salary, salary_upto, salary_currency) => {
     }
 
     return null;
-}
+};
